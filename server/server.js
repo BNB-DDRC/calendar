fb88d5f4e5c670cb12bc8bde19e7101f67e4b38e
Derek Parson <parson.derek@gmail.com>
2019-11-14 17:17:23 -0800

    Add loaderio file in for EC2 instance

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 require('newrelic');
 const express = require('express');
 const path = require('path');
-const compression = require('compression');
-const router = require('./router.js');
+// const compression = require('compression');
+// const router = require('./router.js');
 const bodyParser = require('body-parser');
+const db = require('./postgresql.js')
 
 
 const app = express();
@@ -15,17 +16,18 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use(compression({ filter: shouldCompress }));
+// app.use(compression({ filter: shouldCompress }));
 
-function shouldCompress(req, res) {
-  if (req.headers['x-no-compression']) {
-    // don't compress responses with this request header
-    return false;
-  }
+// function shouldCompress(req, res) {
+//   if (req.headers['x-no-compression']) {
+//     // don't compress responses with this request header
+//     return false;
+//   }
 
-  // fallback to standard filter function
-  return compression.filter(req, res);
-}
+//   // fallback to standard filter function
+//   return compression.filter(req, res);
+// }
+app.use('http://ec2-54-67-123-136.us-west-1.compute.amazonaws.com:3004/loaderio-ec25d5c71ad9a14aefc4b096c68bd4c4.txt', express.static('loaderio-ec25d5c71ad9a14aefc4b096c68bd4c4 (1).txt'))
 
 app.use(bodyParser.json());
 app.use(
@@ -34,11 +36,15 @@ app.use(
   })
 );
 
-//app.use('/', express.static(path.join(__dirname, '..', 'public')));
+app.use('/', express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/rooms/:id', express.static(path.join(__dirname, '..', 'public')));
 
-app.get('/api/:id', router.router);
-app.post('/api/:id', router.router.post);
+app.get('/rooms/:id/availability', (req, res) => {
+  //console.log(req.params);
+  db.getAvailability(req, res)
+});
+
+app.post('/rooms/:id/availability', db.createBooking);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
